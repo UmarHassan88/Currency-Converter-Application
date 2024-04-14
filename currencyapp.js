dcbac7c3ab26ca872e5a3e9197c17de57647faae
UmarHassan88 <syedumarhasanzia@gmail.com>
2024-04-14 20:57:26 +0500

    Initial commit for currency converter

---
+++ currencyapp.js
@@ -0,0 +1,214 @@
+const countryLS = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+  };
+  
+  const api = "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_qJxUTmzUQ5W8hBBKOR2kpRjFbpRXTfPV3nUT9vqk"
+  const x = document.querySelectorAll(".dropdown select");
+  const fromCurr = document.querySelector(".from select");
+  const toCurr = document.querySelector(".to select");
+  for (let i of x){
+      for (code in countryLS){
+  
+  
+      let newOption = document.createElement("option");
+      newOption.innerText = code;
+      newOption.value = code;
+      i.append(newOption);	
+  
+  i.addEventListener("change", (evt)=>{
+      updateFlag(evt.target)    
+        }
+  )
+        }}
+  
+  
+  const updateFlag =  (elem) =>{
+   let val = elem.value;
+   let Currency = countryLS[val];
+   let newSrc = `https://flagsapi.com/${Currency}/flat/64.png`;
+   let img = elem.parentElement.querySelector("img");
+   img.src = newSrc;
+        
+        }
+  
+  const getButton = document.querySelector("button");
+  getButton.addEventListener("click", async(evt) =>{
+  
+         evt.preventDefault();
+         let getValue = document.querySelector("input");
+         let amtvalue = getValue.value;
+         console.log(amtvalue);
+         if (amtvalue == "" || amtvalue < 1){
+  
+        getValue.value = 1;   
+        }
+        const URLcurrencyequiv = `${api}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`
+        let response = await fetch(URLcurrencyequiv); 
+        let data = await response.json()  
+        console.log(response.json());
+         console.log(data[toCurr.value.toLowerCase()]);
+  
+  
+        }
+        
+  
+      )
+  
